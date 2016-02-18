ddd84091d68d0cc115fe9a01814f515b490a400d
David <davidlglozo@gmail.com>
2016-02-18 23:25:17 +0100

    Weather in statusbar[2/3

--- src/com/android/settings/nuclear/StatusBar.java
+++ src/com/android/settings/nuclear/StatusBar.java
@@ -9,6 +9,10 @@ import android.app.ActivityManagerNative;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.Uri;
 import android.text.Spannable;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -22,6 +26,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.preference.PreferenceCategory;
 import android.view.Display;
 import android.view.IWindowManager;
 import android.view.WindowManager;
@@ -35,6 +40,15 @@ import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import com.android.internal.util.omni.PackageUtils;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -47,6 +61,17 @@ import com.android.settings.nuclear.cp.ColorPickerPreference;
 public class StatusBar extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "StatusBar";
+
+    private static final String CATEGORY_WEATHER = "weather_category";
+    private static final String WEATHER_ICON_PACK = "weather_icon_pack";
+    private static final String STATUS_BAR_HEADER_WEATHER = "status_bar_header_weather";
+    private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws";
+    private static final String WEATHER_SERVICE_PACKAGE = "org.omnirom.omnijaws";
+    private static final String LOCK_CLOCK_PACKAGE="com.cyanogenmod.lockclock";
+
+    private PreferenceCategory mWeatherCategory;
+    private ListPreference mWeatherIconPack;
+    private CheckBoxPreference mHeaderWeather;
     
 	private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     private static final String CUSTOM_HEADER_IMAGE = "status_bar_custom_header";
@@ -59,8 +84,6 @@ public class StatusBar extends SettingsPreferenceFragment implements Preference.
     private static final String SHOW_OPERATOR_NAME = "show_operator_name";
 
     private SwitchPreference mStatusBarBrightnessControl;
-    private ListPreference mDaylightHeaderPack;
-    private CheckBoxPreference mCustomHeaderImage;
     private ListPreference mQuickPulldown;
     private SwitchPreference mStatusBarGreeting;
     private SeekBarPreferenceCham mStatusBarGreetingTimeout;
@@ -75,6 +98,8 @@ public class StatusBar extends SettingsPreferenceFragment implements Preference.
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.nuclear_statusbar);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
 
         mStatusBarBrightnessControl = (SwitchPreference) findPreference(STATUS_BAR_BRIGHTNESS_CONTROL);
         mStatusBarBrightnessControl.setOnPreferenceChangeListener(this);
@@ -112,36 +137,12 @@ public class StatusBar extends SettingsPreferenceFragment implements Preference.
         }
 
 
-        final boolean customHeaderImage = Settings.System.getInt(getContentResolver(),
+       /* final boolean customHeaderImage = Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1;
         mCustomHeaderImage = (CheckBoxPreference) findPreference(CUSTOM_HEADER_IMAGE);
-        mCustomHeaderImage.setChecked(customHeaderImage);
+        mCustomHeaderImage.setChecked(customHeaderImage);*/
 
-        String settingHeaderPackage = Settings.System.getString(getContentResolver(),
-                Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK);
-        if (settingHeaderPackage == null) {
-            settingHeaderPackage = DEFAULT_HEADER_PACKAGE;
-        }
-        mDaylightHeaderPack = (ListPreference) findPreference(DAYLIGHT_HEADER_PACK);
-        List<String> entries = new ArrayList<String>();
-        List<String> values = new ArrayList<String>();
-        getAvailableHeaderPacks(entries, values);
-        mDaylightHeaderPack.setEntries(entries.toArray(new String[entries.size()]));
-        mDaylightHeaderPack.setEntryValues(values.toArray(new String[values.size()]));
-
-        int valueIndex = mDaylightHeaderPack.findIndexOfValue(settingHeaderPackage);
-        if (valueIndex == -1) {
-            // no longer found
-            settingHeaderPackage = DEFAULT_HEADER_PACKAGE;
-            Settings.System.putString(getContentResolver(),
-                    Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, settingHeaderPackage);
-            valueIndex = mDaylightHeaderPack.findIndexOfValue(settingHeaderPackage);
-        }
-        mDaylightHeaderPack.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntry());
-        mDaylightHeaderPack.setOnPreferenceChangeListener(this);
-        mDaylightHeaderPack.setEnabled(customHeaderImage);
-   		
+        
    		// logo color
         mLogoColor =
             (ColorPickerPreference) prefSet.findPreference(KEY_LOGO_COLOR);
@@ -165,6 +166,34 @@ public class StatusBar extends SettingsPreferenceFragment implements Preference.
                 Settings.System.STATUS_BAR_GREETING_TIMEOUT, 400);
         mStatusBarGreetingTimeout.setValue(statusBarGreetingTimeout / 1);
         mStatusBarGreetingTimeout.setOnPreferenceChangeListener(this);
+
+        mWeatherCategory = (PreferenceCategory) prefScreen.findPreference(CATEGORY_WEATHER);
+        if (mWeatherCategory != null && !isOmniJawsServiceInstalled()) {
+            prefScreen.removePreference(mWeatherCategory);
+        } else {
+            String settingHeaderPackage = Settings.System.getString(getContentResolver(),
+                    Settings.System.STATUS_BAR_WEATHER_ICON_PACK);
+            if (settingHeaderPackage == null) {
+                settingHeaderPackage = DEFAULT_WEATHER_ICON_PACKAGE;
+            }
+            mWeatherIconPack = (ListPreference) findPreference(WEATHER_ICON_PACK);
+            mWeatherIconPack.setEntries(getAvailableWeatherIconPacksEntries());
+            mWeatherIconPack.setEntryValues(getAvailableWeatherIconPacksValues());
+
+            int valueIndex = mWeatherIconPack.findIndexOfValue(settingHeaderPackage);
+            if (valueIndex == -1) {
+                // no longer found
+                settingHeaderPackage = DEFAULT_WEATHER_ICON_PACKAGE;
+                Settings.System.putString(getContentResolver(),
+                        Settings.System.STATUS_BAR_WEATHER_ICON_PACK, settingHeaderPackage);
+                valueIndex = mWeatherIconPack.findIndexOfValue(settingHeaderPackage);
+            }
+            mWeatherIconPack.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mWeatherIconPack.setSummary(mWeatherIconPack.getEntry());
+            mWeatherIconPack.setOnPreferenceChangeListener(this);
+
+            mHeaderWeather = (CheckBoxPreference) findPreference(STATUS_BAR_HEADER_WEATHER);
+        }
 
     }
 
@@ -219,25 +248,31 @@ public class StatusBar extends SettingsPreferenceFragment implements Preference.
             Settings.System.putInt(getContentResolver(), SHOW_OPERATOR_NAME,
                     value ? 1 : 0);
             return true;
-        } else if (preference == mDaylightHeaderPack) {
+      /*  } else if (preference == mDaylightHeaderPack) {
             String value = (String) objValue;
             Settings.System.putString(getContentResolver(),
                     Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, value);
             int valueIndex = mDaylightHeaderPack.findIndexOfValue(value);
-            mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntries()[valueIndex]);
+            mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntries()[valueIndex]);*/
+        }else if (preference == mWeatherIconPack) {
+            String value = (String) objValue;
+            Settings.System.putString(getContentResolver(),
+                    Settings.System.STATUS_BAR_WEATHER_ICON_PACK, value);
+            int valueIndex = mWeatherIconPack.findIndexOfValue(value);
+            mWeatherIconPack.setSummary(mWeatherIconPack.getEntries()[valueIndex]);
         }
         return true;
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mCustomHeaderImage) {
+       /* if (preference == mCustomHeaderImage) {
             final boolean value = ((CheckBoxPreference)preference).isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_CUSTOM_HEADER, value ? 1 : 0);
             mDaylightHeaderPack.setEnabled(value);
-            return true;
-        } else if (preference == mStatusBarGreeting) {
+            return true;*/
+        if (preference == mStatusBarGreeting) {
            boolean enabled = mStatusBarGreeting.isChecked();
            if (enabled) {
                 AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
@@ -293,43 +328,92 @@ public class StatusBar extends SettingsPreferenceFragment implements Preference.
         }
     }
 
-        private void getAvailableHeaderPacks(List<String> entries, List<String> values) {
+
+private boolean isOmniJawsServiceInstalled() {
+        return PackageUtils.isAvailableApp(WEATHER_SERVICE_PACKAGE, getActivity());
+    }
+
+    private boolean isLockClockInstalled() {
+        return PackageUtils.isAvailableApp(LOCK_CLOCK_PACKAGE, getActivity());
+    }
+
+    private String[] getAvailableWeatherIconPacksValues() {
+        List<String> headerPacks = new ArrayList<String>();
         Intent i = new Intent();
         PackageManager packageManager = getPackageManager();
-        i.setAction("org.omnirom.DaylightHeaderPack");
+        i.setAction("org.omnirom.WeatherIconPack");
         for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
             String packageName = r.activityInfo.packageName;
-            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
-                values.add(0, packageName);
+            if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
+                headerPacks.add(0, r.activityInfo.name);
             } else {
-                values.add(packageName);
+                headerPacks.add(r.activityInfo.name);
             }
-        
+        }
+        if (isLockClockInstalled()) {
+            headerPacks.add(LOCK_CLOCK_PACKAGE + ".weather");
+            headerPacks.add(LOCK_CLOCK_PACKAGE + ".weather_color");
+            headerPacks.add(LOCK_CLOCK_PACKAGE + ".weather_vclouds");
+        }
+        return headerPacks.toArray(new String[headerPacks.size()]);
+    }
+
+    private String[] getAvailableWeatherIconPacksEntries() {
+        List<String> headerPacks = new ArrayList<String>();
+        Intent i = new Intent();
+        PackageManager packageManager = getPackageManager();
+        i.setAction("org.omnirom.WeatherIconPack");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
             String label = r.activityInfo.loadLabel(getPackageManager()).toString();
             if (label == null) {
                 label = r.activityInfo.packageName;
             }
-            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
-                entries.add(0, label);
+            if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
+                headerPacks.add(0, label);
             } else {
-                entries.add(label);
+                headerPacks.add(label);
             }
         }
-        i.setAction("org.omnirom.DaylightHeaderPack1");
-        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
-            String packageName = r.activityInfo.packageName;
-            values.add(packageName  + "/" + r.activityInfo.name);
-
-            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
-            if (label == null) {
-                label = packageName;
-            }
-            entries.add(label);
+        if (isLockClockInstalled()) {
+            headerPacks.add("LockClock (white)");
+            headerPacks.add("LockClock (color)");
+            headerPacks.add("LockClock (vclouds)");
         }
+        return headerPacks.toArray(new String[headerPacks.size()]);
     }
 
+    private void updateWeatherSettings() {
+        final boolean weatherEnabled = isOmniJawsEnabled();
+        if (mHeaderWeather.isChecked() && !weatherEnabled) {
+            // disable if service got disabled
+            Settings.System.putInt(getContentResolver(), Settings.System.STATUS_BAR_HEADER_WEATHER, 0);
+            mHeaderWeather.setChecked(false);
+        }
+        mHeaderWeather.setEnabled(weatherEnabled);
+        mWeatherIconPack.setEnabled(weatherEnabled);
+    }
 
+    private boolean isOmniJawsEnabled() {
+        final Uri SETTINGS_URI
+            = Uri.parse("content://org.omnirom.omnijaws.provider/settings");
 
+        final String[] SETTINGS_PROJECTION = new String[] {
+            "enabled"
+        };
+
+        final Cursor c = getContentResolver().query(SETTINGS_URI, SETTINGS_PROJECTION,
+                null, null, null);
+        if (c != null) {
+            int count = c.getCount();
+            if (count == 1) {
+                c.moveToPosition(0);
+                boolean enabled = c.getInt(0) == 1;
+                return enabled;
+            }
+        }
+         return true;
+     }
 
 
 
